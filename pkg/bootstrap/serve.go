94616cc8339d5be2675929f712c6d851a920d2e0
jocode-1 <akintolajohn41@gmail.com>
2023-10-03 03:12:56 +0100

    Updated Static Files

--- pkg/bootstrap/serve.go
+++ pkg/bootstrap/serve.go
@@ -4,12 +4,14 @@ import (
 	"blog/pkg/config"
 	"blog/pkg/html"
 	"blog/pkg/routing"
+	"blog/pkg/static"
 )
 
 func Serve() {
 
 	config.Set()
 	routing.Init()
+	static.LoadStatic(routing.GetRouter())
 	html.LoadHTML(routing.GetRouter())
 	routing.RegisterRoutes()
 
--- pkg/html/render.go
+++ pkg/html/render.go
@@ -1,12 +1,12 @@
 package html
 
 import (
+	"blog/internal/providers/view"
 	"github.com/gin-gonic/gin"
-	"github.com/spf13/viper"
 )
 
 func Render(c *gin.Context, code int, name string, data gin.H) {
 
-	data["APP_NAME"] = viper.Get("App.Name")
+	data = view.WithGlobalData(data)
 	c.HTML(code, name, data)
 }
